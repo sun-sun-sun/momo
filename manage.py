b0e6f517308a80f838a467e1ca1248c6ec11b85a
sun-sun-sun <904029570@qq.com>
2020-10-22 10:03:40 +0800

    修改测试

--- manage.py
+++ manage.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 import os
 import sys
-
+import sys
+import sys
+import sys
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "MoMo.settings")
     try:
